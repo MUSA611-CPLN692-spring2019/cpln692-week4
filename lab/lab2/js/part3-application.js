9355efe8102dfff61d0345d3c7b9c1b3057406f6
angelicakim28 <hskim28@upenn.edu>
2019-02-15 01:08:18 -0500

    week4 assignments

--- lab/lab2/js/part3-application.js
+++ lab/lab2/js/part3-application.js
@@ -24,13 +24,21 @@
   filters that might actually be useful. Try to see what you can produce.
 ===================== */
 
+var phillyCrimeDataUrl = "https://raw.githubusercontent.com/CPLN692-MUSA611/datasets/master/json/philadelphia-crime-snippet.json";
+
+var currently_plotted_markers = [];
+var currently_selected_data = [];
+var complete_data = [];
+
 /* =====================
   Define a resetMap function to remove markers from the map and clear the array of markers
 ===================== */
-var resetMap = function() {
+var resetMap = function(currently_plotted_markers) {
   /* =====================
     Fill out this function definition
   ===================== */
+  currently_selected_data = complete_data;
+  _.each(currently_plotted_markers, function(i) {return map.removeLayer(i);});
 };
 
 /* =====================
@@ -42,6 +50,18 @@ var getAndParseData = function() {
   /* =====================
     Fill out this function definition
   ===================== */
+  $.ajax(phillyCrimeDataUrl).done(function(ajaxResponseValue){
+    // array of objects into json
+    complete_data = JSON.parse(ajaxResponseValue);
+      // for(var i = 0; i < complete_data.length; i++){
+      //   console.log(complete_data[i]);
+      // }
+
+    //make markers and map function to all points and add to map
+    // var makeMarkers = (i) => {return L.marker([i.Y,i.X]).bindPopup(i.NAME).addTo(map);};
+    // var allMarkers = _.map(computedValue, makeMarkers);
+
+  });
 };
 
 /* =====================
@@ -50,6 +70,64 @@ var getAndParseData = function() {
 ===================== */
 var plotData = function() {
   /* =====================
-    Fill out this function definition
+    data:
+      numericField1
+      numericField2
+      booleanField
+      stringField
   ===================== */
+  
+  // console.log(currently_selected_data[0]["Dispatch Date/Time"].substring(20,22)==="PM");
+  if(numericField1 != '' && numericField2 != ''){
+    currently_selected_data = _.filter(currently_selected_data, function(val) {
+      var cur_time = val["Dispatch Date/Time"];
+      var cur_hour = parseInt(cur_time.substring(11,13));
+      var ampm = cur_time.substring(20,22);
+      if(ampm === "PM"){
+        cur_hour += 12;
+      }
+
+      var cur_t = new Date('December 17, 1995 00:00:00');
+      cur_t.setHours(cur_hour,parseInt(cur_time.substring(14,16)),parseInt(cur_time.substring(17,19)));
+
+      var n1_t = new Date('December 17, 1995 00:00:00');
+      n1_t.setHours(numericField1);
+
+      var n2_t = new Date('December 17, 1995 00:00:00');
+      n2_t.setHours(numericField2);
+
+      if((cur_t >= n1_t && cur_t <= n2_t)){
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  if(stringField != ''){
+    currently_selected_data = _.filter(currently_selected_data, function(val) {
+      return (val["General Crime Category"] == stringField);
+    });
+  }
+
+  if(booleanField == true){
+      currently_selected_data=_.filter(currently_selected_data,function(val){
+        var crime_location = val["Location Block"];
+        var south_broad_st = "S BROAD ST";
+        var oregon_av = "OREGON AV";
+        var oregon_ave = "OREGON AVE";
+        if (crime_location.includes(south_broad_st) | crime_location.includes(oregon_av) | crime_location.includes(oregon_ave)){
+          console.log(crime_location);
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+
+  var makeMarkers = (i) => {return L.marker([i.Lat,i.Lng]).bindPopup(i["Location Block"]).addTo(map);};
+  var currently_plotted_markers = _.map(currently_selected_data, makeMarkers);
+
+
+  return currently_plotted_markers;
 };
